d2203126da0b8462229cb5a405dcd7e711a8a7f2
omrr-ygc <omrr.ygc@gmail.com>
2025-04-24 03:57:56 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Omer Yagci',
+    author_email= 'omrr.ygc@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
